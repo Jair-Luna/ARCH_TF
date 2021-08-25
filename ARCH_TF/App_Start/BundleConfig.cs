bf492a1fa39754b9dc9fd554e0ec6145f035a4e3
Jair <lunajair90@gmail.com>
2021-08-24 21:56:47 -0500

    v0.0.3 Bootstrap

--- ARCH_TF/App_Start/BundleConfig.cs
+++ ARCH_TF/App_Start/BundleConfig.cs
@@ -23,8 +23,8 @@ namespace ARCH_TF
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/bootstrap.css",
+                      "~/Content/css/site.css"));
         }
     }
 }
