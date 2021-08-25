50d0235a8b23cd6466c8357c15a38724cdfb54f0
Jair <lunajair90@gmail.com>
2021-08-25 18:09:42 -0500

    Primer Login

---
+++ ARCH_TF/Controllers/UsuarioController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ARCH_TF.Controllers
+{
+    
+    public class UsuarioController : Controller
+    {
+
+        // GET: Usuario
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
