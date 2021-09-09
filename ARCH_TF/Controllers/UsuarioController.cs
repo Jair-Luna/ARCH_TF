6233c27ffdd7fc0c391c4642fde2288cb41ee47f
Jair Alexander Luna <lunajair90@gmail.com>
2021-09-08 22:54:58 -0500

    Solicitudes y Quejas

--- ARCH_TF/Controllers/UsuarioController.cs
+++ ARCH_TF/Controllers/UsuarioController.cs
@@ -14,17 +14,52 @@ namespace ARCH_TF.Controllers
         private DB_HidrocarburosEntities db = new DB_HidrocarburosEntities();
 
         // GET: Usuario
-        public ActionResult Index()
+        public ActionResult Index( string message = "")
         {
+            ViewBag.Message = message;
+            
             var email = User.Identity.Name;
             var user = db.usuario.FirstOrDefault(e => e.email == email);
 
             return View(user);
         }
+
         public ActionResult Solicitud()
         {
             var email = User.Identity.Name;
             var user = db.usuario.FirstOrDefault(e => e.email == email);
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult CrearSolicitud(string descripcion)
+        {
+            var email = User.Identity.Name;
+            var user = db.usuario.FirstOrDefault(e => e.email == email);
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    using (DB_HidrocarburosEntities db = new DB_HidrocarburosEntities())
+                    {
+                        formularios d = new formularios();
+                        d.cedula_usuario = user.cedula_usuario;
+                        d.tipo_formulario = 2;
+                        d.descripcion = descripcion;
+
+                        db.formularios.Add(d);
+                        db.SaveChanges();
+
+                        return RedirectToAction("Index", new { message = "Su formulario se ha enviado con exito" });
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+
+            }
 
             return View(user);
         }
@@ -33,6 +68,39 @@ namespace ARCH_TF.Controllers
         {
             var email = User.Identity.Name;
             var user = db.usuario.FirstOrDefault(e => e.email == email);
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult CrearQueja(string descripcion)
+        {
+            var email = User.Identity.Name;
+            var user = db.usuario.FirstOrDefault(e => e.email == email);
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    using (DB_HidrocarburosEntities db = new DB_HidrocarburosEntities())
+                    {
+                        formularios d = new formularios();
+                        d.cedula_usuario = user.cedula_usuario;
+                        d.tipo_formulario = 3;
+                        d.descripcion = descripcion;
+
+                        db.formularios.Add(d);
+                        db.SaveChanges();
+
+                        return RedirectToAction("Index", new { message = "Su formulario se ha enviado con exito" });
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+
+            }
 
             return View(user);
         }
